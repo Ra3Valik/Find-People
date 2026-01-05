6f8db26156d555ed90efb39cdaa4037abf55edd6
Ra3Valik <Ldidi1@yandex.ru>
2026-01-05 19:22:42 +0300

    Update welcome page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,10 +6,28 @@
     <div class="p-6">
         <h1 class="text-2xl font-semibold">{{ __( 'front/home.welcome' ) }}</h1>
 
-        @if($canRegister ?? false)
-            <a class="underline" href="{{ route('register') }}">{{ __( 'front/auth.register' )  }}</a>
-        @endif
+        @if( auth()->user() )
+            <span class="admin-bar__hello">
+                {{ __('general.hello') }},
+                <span class="admin-bar__name">{{ auth()->user()->name }}</span>
+            </span>
 
-        <a class="underline ml-4" href="{{ route('login') }}">{{ __( 'front/auth.login' )  }}</a>
+
+            <form method="POST" action="{{ route('logout') }}" class="admin-bar__logout-form">
+                @csrf
+                <a href="{{ route('logout') }}"
+                   class="admin-bar__link admin-bar__link--danger"
+                   onclick="event.preventDefault(); this.closest('form').submit();"
+                >
+                    {{ __('front/auth.logout') }}
+                </a>
+            </form>
+        @else
+            @if($canRegister ?? false)
+                <a class="underline" href="{{ route('register') }}">{{ __( 'front/auth.register' )  }}</a>
+            @endif
+
+            <a class="underline ml-4" href="{{ route('login') }}">{{ __( 'front/auth.login' )  }}</a>
+        @endif
     </div>
 @endsection
